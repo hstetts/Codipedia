ed9612ccec59b20a633bc70a76a0e91abeefa4b2
hstetts@gmail.com <hstetts@gmail.com>
2018-02-24 12:06:55 -0500

    Updated heroku app name to codipedia

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'guarded-badlands-58905.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'codipedia.herokuapp.com' }
 end
